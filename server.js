7e79ebb5de7d889757100e4f0cf294a181695b2e
Haroldas <haro.lato@gmail.com>
2015-11-24 21:09:35 +0000

    log

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
+    console.log("listening to: " + port);
 }).listen(port);
